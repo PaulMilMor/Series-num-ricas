b92e19a569f80c2da4cc3fdd515638a0d2545296
Paúl Millanes Morimoto <pauljmm99@gmail.com>
2020-02-21 07:46:47 +0000

    finished

--- Main.java
+++ Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 class Main {
   public static void main(String[] args) {
    
@@ -10,19 +12,85 @@ class Main {
      System.out.println("4- Salir");
      Scanner sc = new Scanner(System.in);
      String option = sc.next();
+     try{
      if(option.equals("1")){
-       
+       System.out.println("Introduzca el número de elementos");
+       String limit = sc.next();
+       int limitint = Integer.parseInt(limit);
+        fibonacci(limitint);
      } else if(option.equals("2")){
-       
+       System.out.println("Introduzca el número de elementos");
+       String limit = sc.next();
+       int limitint = Integer.parseInt(limit);
+       for(int i = 1; i<= limitint;i++){
+        System.out.print(padovan(i)+", ");
+      }
+      System.out.println();
+      
      } else if(option.equals("3")){
-       
+       System.out.println("Introduzca el número de renglones");
+       String limit = sc.next();
+       int limitint = Integer.parseInt(limit);
+       pascal(limitint);
      } else if(option.equals("4")){
+       System.out.println("bye bye");
+       sc.close();
        break;
      } else{
        System.out.println("Introduzca una opción válida");
      }
-      
+     } catch(NumberFormatException e){
+       System.out.println("Introduczca un límite válido");
+     }
      
    }
+  }
+  
+  public static int padovan(int limit){
+    
+    if(limit>3){
+      
+      return padovan(limit-2) + padovan(limit-3);
+    } else if(limit<4 && limit>0){
+      return 1;
+    } else{
+      return -1;
+    }
+  }
+
+  public static void fibonacci(int limit){
+    int t1 = 0, t2 = 1;
+        for (int i = 1; i <= limit; ++i)
+        {
+            System.out.print(t1 + ", ");
+
+            int sum = t1 + t2;
+            t1 = t2;
+            t2 = sum;
+        }
+        System.out.println();
+  }
+  public static void pascal(int limit){
+    for(int i = 0; i < limit; i++) {
+         for(int j = 0; j <= limit-i; j++){
+            System.out.print(" ");
+         }
+         for(int j = 0; j <= i; j++){
+            int facti,i2=i,ij=i-j, factij, j2=j,factj;
+            for(facti=1;i2>1;i2--){
+              facti*=i2;
+            }
+            for(factij=1;ij>1;ij--){
+              factij*=ij;
+            }
+            for(factj=1;j2>1;j2--){
+              factj*=j2;
+            }
+            //System.out.println("facti:" + facti + "factij:" + factij + "factj:" + factj);
+            int ncr = facti / ( factij * factj);
+            System.out.print(" "+ncr);
+         }
+         System.out.println();
+      }
   }
 }
